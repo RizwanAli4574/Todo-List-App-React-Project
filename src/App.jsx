7ba793b9a1bed1348b81b036d651f2c30e6d6843
rizwanAli <rizwanali4574992@gmail.com>
2024-11-06 22:46:19 +0500

    remove pedding

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Todo from "./Components/Todo";
 const App = () => {
   return (
     // <div className="bg-stone-900 py-4 min-h-screen ">
-    <div className="bg-stone-900 py-4 min-h-screen flex items-center justify-center">
+    <div className="bg-stone-900  min-h-screen flex items-center justify-center">
       <Todo />
     </div>
   );
